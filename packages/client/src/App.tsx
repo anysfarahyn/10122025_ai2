45d1a1b231f5b7fddb815924690eb64eca022efb
Anis.Mahadir <anis.mahadir@kinesso.com>
2025-12-16 10:33:12 +0800

    connect the frontend and backend

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+    .then(res => res.json())
+    .then(data => setMessage(data.message));
+  }, []);
+   return <p>{message}</p>
+}
+
+export default App
+  
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import type { Request, Response } from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -8,9 +8,13 @@ const app = express();
 const port = process.env.PORT || 300;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send("Hello World!");
+});
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello World!" });
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-})
+});
